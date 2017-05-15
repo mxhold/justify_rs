3054dcc508077bf86f33c14e85ff790f829f4c96
Holder <code@getbraintree.com>
2017-05-15 09:25:21 -0700

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 extern crate rand;
-use rand::{sample, weak_rng, Rng, XorShiftRng, SeedableRng};
+use rand::{sample, weak_rng, XorShiftRng, SeedableRng};
 // pub fn justify(input: &str, width: usize) -> Vec<String> {
 //     let mut line_words: Vec<String> = Vec::new();
 //     let mut lines: Vec<String> = Vec::new();
@@ -28,15 +28,25 @@ fn get_rng(seed: Option<[u32; 4]>) -> XorShiftRng {
 }
 
 pub fn justify_line(line: &str, desired_length: usize, seed: Option<[u32; 4]>) -> String {
+    if line.len() == desired_length {
+        return line.to_string();
+    }
+
     let words: Vec<&str> = line.split_whitespace().collect();
 
     let word_length: usize = words.iter().map(|word| word.len()).sum();
+
     let spaces_to_add = desired_length - word_length;
 
     let word_count: usize = words.len();
     let space_position_count = word_count - 1;
 
+    if space_position_count <= 0 {
+        return line.to_string();
+    }
+
     let spaces_for_each_space_position = spaces_to_add / space_position_count;
+    assert!(spaces_for_each_space_position >= 1);
     let extra_spaces = spaces_to_add % space_position_count;
 
     let space_positions = 0..space_position_count;
@@ -65,13 +75,13 @@ pub fn justify_line(line: &str, desired_length: usize, seed: Option<[u32; 4]>) -
 mod tests {
     use justify_line;
 
-    // #[test]
-    // fn it_adds_spaces_to_fit_width() {
-    //     let input = "123 456";
-    //     let result = justify_line(input, 8);
-    //
-    //     assert_eq!(result, "123  456");
-    // }
+    #[test]
+    fn it_adds_spaces_to_fit_desired_length() {
+        let input = "123 456";
+        let result = justify_line(input, 8, None);
+
+        assert_eq!(result, "123  456");
+    }
 
     #[test]
     fn it_adds_extra_spaces_randomly() {
@@ -84,5 +94,28 @@ mod tests {
         let result = justify_line(input, 12, Some([2, 2, 3, 4]));
 
         assert_eq!(result, "123 456  789");
+    }
+
+    #[test]
+    fn it_returns_input_if_already_at_desired_length() {
+        let input = "1 2";
+        let result = justify_line(input, 3, None);
+
+        assert_eq!(result, "1 2");
+    }
+
+    #[test]
+    fn it_returns_input_if_one_word() {
+        let input = "123";
+        let result = justify_line(input, 4, None);
+
+        assert_eq!(result, "123");
+    }
+
+    #[test]
+    #[should_panic]
+    fn it_panics_if_input_longer_than_desired_length() {
+        let input = "1 3";
+        justify_line(input, 2, None);
     }
 }
